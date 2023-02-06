1fdb3dae6bcabcc83ff47e962e0fce7d9e27a31f
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-02-06 14:49:19 -0500

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -2,28 +2,31 @@
 #include <string>
 #include "book.hpp"
 
-    void Book::setTitle(std::string getTitle){
+    void Book::setTitle(const std::string getTitle){
         book_title = getTitle;
-    }
+    };
     std::string Book::getTitle() const{
         return book_title;
-    }
+    };
 
-    void Book::setAuthor(std::string getAuthor){
+    void Book::setAuthor(const std::string getAuthor){
         book_author = getAuthor;
-    }
+    };
+
     std::string Book::getAuthor() const{
         return book_author;
-    }
+    };
 
-    void Book::setPageCount(int getPageCount){
+    void Book::setPageCount(const int getPageCount){
         if(getPageCount > 0){
             page_count = getPageCount;
         }
-    }
+    };
+
     int Book::getPageCount() const{
         return page_count;
-    }
+    };
+
     void Book::setDigital(bool isDigital){
         if(isDigital == 1){
             digital = true;
@@ -31,7 +34,9 @@
         else{
             digital = false;
         }
-    }
+    };
+    
     bool Book::isDigital() const{
         return digital;
-    }
+    }; 
+      
