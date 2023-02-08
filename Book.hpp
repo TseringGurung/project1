499c6a122ea530a56e89ff1fbe598c0808178295
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-02-07 22:45:07 -0500

    Update Book.hpp

--- Book.hpp
+++ Book.hpp
@@ -2,22 +2,45 @@
 #define BOOK_H
 
 class Book{
+/*private:
+    - The title of the book (a string)
+    - The author of the book (a string)
+    - The page count (an integer)
+    - A flag indicating whether the book is available in digital form (a Boolean)*/
 private:
     std::string book_title;
     std::string book_author;
     int page_count;
     bool digital;
 public:
+    /**
+      Default constructor.
+      Default-initializes all private members. Booleans are default-initialized to False.
+   */
     Book();
-    Book(const std::string getTitle,const std::string getAuthor,const int getPageCount,const bool isDigital = false);
 
-    void setTitle(const std::string getTitle);
-    void setAuthor(const std::string getAuthor);
-    void setPageCount(const int getPageCount);
-    void setDigital();
+   /**
+      Parameterized constructor.
+      @param      : The title of the book (a string)
+      @param      : The author of the book (a string)
+      @param      : The number of pages in the book (a positive int)
+      @param      : A flag indicating whether the book is in digital form (a Boolean),
+                    with default value False
+      @post       : The private members are set to the values of the corresponding parameters.
+    */
+    
+    Book(const std::string Title,const std::string Author,const int PageCount,const bool Digital = false);
+
+    void setTitle(const std::string Title);
     std::string getTitle() const;
+    
+    void setAuthor(const std::string Author);
     std::string getAuthor() const;
+   
+    void setPageCount(const int PageCount);
     int getPageCount() const;
+
+    void setDigital();
     bool isDigital() const; 
 }; 
 
